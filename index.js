f206f6cf06529e44b3a226c7c87e19814b765f82
Rachel Cherry <bamadesigner@gmail.com>
2020-04-18 15:45:25 -0400

    Remove @wpcampus/wpcampus-wc-library

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("@wpcampus/wpcampus-wc-blog");
 require("@wpcampus/wpcampus-wc-conduct");
 require("@wpcampus/wpcampus-wc-footer");
-require("@wpcampus/wpcampus-wc-library");
 require("@wpcampus/wpcampus-wc-notifications");
